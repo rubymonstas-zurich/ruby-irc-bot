5c2c7d3e2bc7ff6e1c82ea5ac4e1afbf4a195d26
Thomas Ritter <nethad@gmx.net>
2015-07-24 11:28:31 +0200

    Do you even brackets?

--- irc_bot.rb
+++ irc_bot.rb
@@ -7,9 +7,9 @@ def run
     message = @socket.gets
     puts message
 
-    if message.start_with? "PING"
+    if message.start_with?("PING")
       handle_ping_message(message)
-    elsif message.include? "PRIVMSG #{@channel}"
+    elsif message.include?("PRIVMSG #{@channel}")
       handle_channel_message(message)
     end
   end
@@ -17,19 +17,19 @@ end
 
 def connect
   @socket = TCPSocket.open(@server, @port)
-  irc_send "NICK #{@name}"
-  irc_send "USER #{@name} 0 * :#{@name}"
-  irc_send "JOIN #{@channel}"
+  irc_send("NICK #{@name}")
+  irc_send("USER #{@name} 0 * :#{@name}")
+  irc_send("JOIN #{@channel}")
 end
 
 def irc_send(message)
   puts "Sending: #{message}"
-  @socket.puts message
+  @socket.puts(message)
 end
 
 def handle_ping_message(message)
   hostname = message.split(" ").last
-  irc_send "PONG #{hostname}"
+  irc_send("PONG #{hostname}")
 end
 
 def handle_channel_message(message)
