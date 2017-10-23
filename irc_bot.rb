fef74ace45f984cf6ba666584d94fb9f8f3d0847
Ferdinand Niedermann <ferdinand.niedermann@gmail.com>
2017-10-23 18:07:37 +0200

    Update irc_bot.rb

--- irc_bot.rb
+++ irc_bot.rb
@@ -1,13 +1,15 @@
-###############################################
-# Implement your own ideas below this comment #
-###############################################
-
+# customize the name of your bot on the following line
 @name = "your-name-bot"
 
 # This method gets called, whenever a message is sent to our IRC channel. In it you can react to
 # the users' inputs in whatever way you like...
 def handle_channel_message(message)
-  # This method is empty. Now it's your turn.
+  # Implement your ideas here. 
+  # The string in `message` will look something like this: 
+  # ":nerdinand!b2c5e1f5@gateway/web/freenode/ip.178.197.225.245 PRIVMSG #rubymonstas :Look at me, I'm here!"
+  # 
+  # You can use the method `irc_send` to send strings to the IRC server, e.g.
+  # irc_send("PRIVMSG #rubymonstas :Hi everyone!")
 end
 
 require_relative './irc_magic.rb'
